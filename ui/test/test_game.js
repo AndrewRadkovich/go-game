154f9745fc4afca098c24086f5564db3f79af69a
Andrei Radkovich <andrei_radkovich@epam.com>
2017-05-13 00:09:14 +0300

    fixed all tests

--- ui/test/test_game.js
+++ ui/test/test_game.js
@@ -7,9 +7,8 @@ const assert = require('chai').assert;
 
 describe("Game", () => {
   it("player has been switched after move", () => {
-
     const eventbus = new EventBus();
-    const board = new Board(19, eventbus);
+    const board = new Board(19, 10, eventbus);
     let game = new Game(board, eventbus).init();
 
     let stone = { x: 1, y: 1, color: 'black' };
@@ -20,23 +19,21 @@ describe("Game", () => {
   });
 
   it("should create one cluster of single black stone after first move", () => {
-
     const eventbus = new EventBus();
-    const board = new Board(19, eventbus);
+    const board = new Board(19, 10, eventbus);
     let game = new Game(board, eventbus).init();
 
     let stone = { x: 1, y: 1, color: 'black' };
 
     eventbus.publish("player.made.a.move", stone);
     assert.equal(game.stoneClusters[stone.color].length, 1);
-    assert.equal(game.stoneClusters[stone.color][0].length, 1);
-    assert.deepEqual(game.stoneClusters[stone.color][0][0], { x: 1, y: 1 });
+    assert.equal(game.stoneClusters[stone.color][0].stones.length, 1);
+    assert.deepEqual(game.stoneClusters[stone.color][0].stones[0], { x: 1, y: 1 });
   });
 
   it("should create two clusters each with one stone", () => {
-
     const eventbus = new EventBus();
-    const board = new Board(19, eventbus);
+    const board = new Board(19, 10, eventbus);
     let game = new Game(board, eventbus).init();
 
     let stone1 = { x: 1, y: 1, color: 'black' };
@@ -47,29 +44,28 @@ describe("Game", () => {
     eventbus.publish("player.made.a.move", stone1);
     assert.equal(game.stoneClusters['black'].length, 1);
     assert.equal(game.stoneClusters['white'].length, 0);
-    assert.deepEqual(game.stoneClusters['black'][0][0], { x: 1, y: 1 });
+    assert.deepEqual(game.stoneClusters['black'][0].stones[0], { x: 1, y: 1 });
 
     eventbus.publish("player.made.a.move", stone2);
     assert.equal(game.stoneClusters['white'].length, 1);
-    assert.deepEqual(game.stoneClusters['white'][0][0], { x: 0, y: 1 });
+    assert.deepEqual(game.stoneClusters['white'][0].stones[0], { x: 0, y: 1 });
 
     eventbus.publish("player.made.a.move", stone3);
     assert.equal(game.stoneClusters['black'].length, 1);
-    assert.equal(game.stoneClusters['black'][0].length, 2);
-    assert.deepEqual(game.stoneClusters['black'][0][0], { x: 1, y: 1 });
-    assert.deepEqual(game.stoneClusters['black'][0][1], { x: 0, y: 0 });
+    assert.equal(game.stoneClusters['black'][0].stones.length, 2);
+    assert.deepEqual(game.stoneClusters['black'][0].stones[0], { x: 1, y: 1 });
+    assert.deepEqual(game.stoneClusters['black'][0].stones[1], { x: 0, y: 0 });
 
     eventbus.publish("player.made.a.move", stone4);
     assert.equal(game.stoneClusters['black'].length, 2);
-    assert.equal(game.stoneClusters['black'][0].length, 2);
-    assert.equal(game.stoneClusters['black'][1].length, 1);
-    assert.deepEqual(game.stoneClusters['black'][1][0], { x: 3, y: 3 });
+    assert.equal(game.stoneClusters['black'][0].stones.length, 2);
+    assert.equal(game.stoneClusters['black'][1].stones.length, 1);
+    assert.deepEqual(game.stoneClusters['black'][1].stones[0], { x: 3, y: 3 });
   });
 
   it("It create should one cluster", () => {
-
     const eventbus = new EventBus();
-    const board = new Board(19, eventbus);
+    const board = new Board(19, 10, eventbus);
     const game = new Game(board, eventbus).init();
 
     board.placeStone({ x: 1, y: 1 }, "white");
@@ -77,9 +73,8 @@ describe("Game", () => {
   });
 
   it("It should create 2 clusters", () => {
-
     const eventbus = new EventBus();
-    const board = new Board(19, eventbus);
+    const board = new Board(19, 10, eventbus);
     const game = new Game(board, eventbus).init();
 
     board.placeStone({ x: 1, y: 1 }, "white");
@@ -88,9 +83,8 @@ describe("Game", () => {
   });
 
   it("It should merge two stone clusters in one after they are connected", () => {
-
     const eventbus = new EventBus();
-    const board = new Board(19, eventbus);
+    const board = new Board(19, 10, eventbus);
     const game = new Game(board, eventbus).init();
 
     board.placeStone({ x: 1, y: 1 }, "white");
